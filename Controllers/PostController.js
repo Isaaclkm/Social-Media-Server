45d382f3ef3e29f3bdf9ea1d11cf1d2eefd0e9f2
Isaaclkm <isaacfelixmatos@gmail.com>
2022-12-02 20:06:48 -0500

    after like

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -37,7 +37,32 @@ export const updatePost = async(req, res) =>{
             await post.updateOne ( {$set: req.body})
             res.status(200).json("Post Updated")
         }
+        else{
+            res.status(403).json("Action forbidden")
+        }
     } catch (error) {
         res.status(500).json(error)
+    }
+}
+
+// Delete a post 
+export const deletePost = async(req, res) => {
+    const id = req.params.id;
+    const {userId} = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+        if(post.userId === userId)
+        {
+            await post.deleteOne();
+            res.status(200).json("Post deleted succesfully")
+
+        }  
+        else{
+            res.status(403).json("Action forbidden")
+        }      
+
+    } catch (error) {
+        
     }
 }
