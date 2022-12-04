3e14d48a2607c9f5900ded255423e2190d250744
Isaaclkm <isaacfelixmatos@gmail.com>
2022-12-03 20:39:46 -0500

    fisnished the mongo part

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,4 +1,6 @@
-import PostModel from "../Models/postModel.js"
+import PostModel from "../Models/postModel.js";
+import mongoose from "mongoose";
+import UserModel from "../Models/userModel.js";
 
 //Create new post
 export const createPost = async(req, res) =>{
@@ -90,3 +92,38 @@ export const likePost = async (req, res) => {
     }
 }
 
+//Get Timeline Post
+
+export const getTimelinePosts = async(req, res) =>{
+    const userId = req.params.id 
+
+    try {
+        const currentUserPosts = await PostModel.find({userId: userId}) 
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: { _id: new mongoose.Types.ObjectId(userId)}
+            },
+            {
+                $lookup: {
+                    from: "posts",
+                    localField: "following",
+                    foreignField: "userId",
+                    as: "followingPosts"
+                }
+            },
+            {
+                $project:{
+                    followingPosts : 1,
+                    _id: 0
+                }
+            }
+        ])
+        res.status(200)
+        .json(currentUserPosts.concat(...followingPosts[0].followingPosts))
+        .sort((a,b)=>{
+            return b.createdAt - a.createdAt
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
