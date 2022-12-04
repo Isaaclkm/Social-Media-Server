0a3301c4de6d141d3768e5487b83bdfff6b9d0ed
Isaaclkm <isaacfelixmatos@gmail.com>
2022-12-03 19:46:36 -0500

    mother

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -68,3 +68,25 @@ export const deletePost = async(req, res) => {
 }
 
 //like / dislike a post
+
+export const likePost = async (req, res) => {
+    const id = req.params.id 
+    const { userId } = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+        if(!post.likes.includes(userId))
+        {
+            await post.updateOne({$push : {likes: userId}})
+            res.status(200).json("Post Liked")
+        }
+        else{
+            await post.updateOne({$pull : {likes: userId}})
+            res.status(200).json("Post Unliked")
+        }
+
+    } catch (error) {
+        
+    }
+}
+
